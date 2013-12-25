0c41c8789663d2679c8156dd726ef0f8bd3cb8c5
Ery Lee <ery.lee@gmail.com>
2013-12-25 19:03:38 +0800

    blueprint

--- webapp.py
+++ webapp.py
@@ -1,81 +1,15 @@
 
 from flask import Flask, render_template, jsonify, make_response
 
+from webim.views import webim_view
+
 app = Flask(__name__)
 app.config.from_pyfile("setting.cfg")
-
-#config from setting.py
+app.register_blueprint(webim_view, url_prefix='/webim')
 
 @app.route("/")
 def index():
 	return render_template("index.html");
-
-@app.route("/webim/boot")
-def boot():
-	urlpath = "http://localhost:5000"
-	response = make_response(render_template('/webim/boot.js', urlpath=urlpath))
-	response.headers['Content-Type'] = 'text/javascript'
-	return response
-
-@app.route("/webim/online", methods=['POST'])
-def online():
-	return jsonify({'success':False})
-
-@app.route("/webim/offline", methods=['POST'])
-def offline():
-	return jsonify("ok")
-
-@app.route("/webim/refresh", methods=['POST'])
-def deactivate():
-	return jsonify("ok")
-
-@app.route("/webim/message", methods=['POST'])
-def message():
-	return jsonify("ok")
-
-@app.route("/webim/presence", methods=['POST'])
-def presence():
-	return jsonify("ok")
-
-@app.route("/webim/status", methods=['POST'])
-def status():
-	return jsonify("ok")
-
-@app.route("/webim/setting", methods=['POST'])
-def setting():
-	return "ok"
-	
-@app.route("/webim/history")
-def history():
-	return jsonify("ok")
-
-@app.route("/webim/history/clear", methods=['POST'])
-def clear_history():
-	return jsonify("ok")
-
-@app.route("/webim/history/download", methods=['POST'])
-def download_history():
-	return jsonify("ok")
-
-@app.route("/webim/members")
-def members():
-	return jsonify("ok")
-
-@app.route("/webim/group/join")
-def join_group():
-	return jsonify("ok")
-
-@app.route("/webim/group/leave")
-def leave_group():
-	return jsonify("ok")
-
-@app.route("/webim/buddies")
-def buddies():
-	return jsonify("ok")
-
-@app.route("/webim/notifications")
-def notifications():
-	return jsonify([])
 
 if __name__ == "__main__":
 	app.run()
---
+++ webim/views.py
@@ -0,0 +1,74 @@
+from flask import Blueprint, render_template, jsonify, current_app, make_response
+
+
+webim_view = Blueprint('webim_view', __name__)
+
+@webim_view.route("/boot")
+def boot():
+	#WebIM config
+	WEBIM_CFG = current_app.config['WEBIM']
+	#TODO:
+	urlpath = "http://localhost:5000"
+	response = make_response(render_template('/webim/boot.js', urlpath=urlpath, WEBIM_CFG=WEBIM_CFG))
+	response.headers['Content-Type'] = 'text/javascript'
+	return response
+
+@webim_view.route("/online", methods=['POST'])
+def online():
+	return jsonify({'success':False})
+
+@webim_view.route("/offline", methods=['POST'])
+def offline():
+	return jsonify("ok")
+
+@webim_view.route("/refresh", methods=['POST'])
+def deactivate():
+	return jsonify("ok")
+
+@webim_view.route("/message", methods=['POST'])
+def message():
+	return jsonify("ok")
+
+@webim_view.route("/presence", methods=['POST'])
+def presence():
+	return jsonify("ok")
+
+@webim_view.route("/status", methods=['POST'])
+def status():
+	return jsonify("ok")
+
+@webim_view.route("/setting", methods=['POST'])
+def setting():
+	return "ok"
+	
+@webim_view.route("/history")
+def history():
+	return jsonify("ok")
+
+@webim_view.route("/history/clear", methods=['POST'])
+def clear_history():
+	return jsonify("ok")
+
+@webim_view.route("/history/download", methods=['POST'])
+def download_history():
+	return jsonify("ok")
+
+@webim_view.route("/members")
+def members():
+	return jsonify("ok")
+
+@webim_view.route("/group/join")
+def join_group():
+	return jsonify("ok")
+
+@webim_view.route("/group/leave")
+def leave_group():
+	return jsonify("ok")
+
+@webim_view.route("/buddies")
+def buddies():
+	return jsonify("ok")
+
+@webim_view.route("/notifications")
+def notifications():
+	return jsonify([])
