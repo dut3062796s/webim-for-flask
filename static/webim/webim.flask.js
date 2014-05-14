3ecc6009e0b4c13e38c07175f0f97c8c7d9c3324
Ery Lee <ery.lee@gmail.com>
2014-05-14 23:57:35 +0800

    update static

--- static/webim/webim.flask.js
+++ static/webim/webim.flask.js
@@ -9,7 +9,10 @@
 (function(webim) {
 	var path = _IMC.path;
 	webim.extend(webim.setting.defaults.data, _IMC.setting);
-	var webim = window.webim;
+    var cookie_key = "_webim_cookie_";
+	if( _IMC.is_visitor ) { cookie_key = "_webim_v_cookie_"; }
+    if( _IMC.user != "" ) { cookie_key = cookie_key + _IMC.user.id; }
+    webim.status.defaults.key = cookie_key;
 	//configure api routes
 	webim.route( {
 		online: path + "/webim/online",
@@ -22,11 +25,18 @@
 		history: path + "/webim/history",
 		clear: path + "/webim/history/clear",
 		download: path + "/webim/history/download",
-		members: path + "/webim/members",
-		join: path + "/webim/group/join",
-		leave: path + "/webim/group/leave",
 		buddies: path + "/webim/buddies",
-		notifications: path + "/webim/notifications"
+        //room actions
+		invite: path + "/webim/room/invite",
+		join: path + "/webim/room/join",
+		leave: path + "/webim/room/leave",
+		block: path + "/webim/room/block",
+		unblock: path + "/webim/room/unblock",
+		members: path + "/webim/room/members",
+        //notifications
+		notifications: path + "/webim/notifications",
+        //upload files
+		upload: path + "/webim/upload"
 	} );
 
 	//configure emotion icons
@@ -44,15 +54,22 @@
 			jsonp: _IMC.jsonp
 		},
 		soundUrls: soundUrls,
+		//layout: "layout.popup",
+        layoutOptions: {
+            unscalable: _IMC.is_visitor
+        },
 		buddyChatOptions: {
             downloadHistory: !_IMC.is_visitor,
-			simple: _IMC.is_visitor,
+			//simple: _IMC.is_visitor,
 			upload: _IMC.upload && !_IMC.is_visitor
 		},
 		roomChatOptions: {
+            downloadHistory: !_IMC.is_visitor,
 			upload: _IMC.upload
 		}
 	}), im = ui.im;
+    //全局化
+    window.webimUI = ui;
 
 	if( _IMC.user ) im.setUser( _IMC.user );
 	if( _IMC.menu ) ui.addApp("menu", { "data": _IMC.menu } );
@@ -64,21 +81,18 @@
 		is_login: _IMC['is_login'],
 		disable_login: true,
 		collapse: false,
-		disable_user: _IMC.is_visitor,
-        simple: _IMC.is_visitor,
+		//disable_user: _IMC.is_visitor,
+        //simple: _IMC.is_visitor,
 		loginOptions: _IMC['login_options']
-	} );
+	});
     if(!_IMC.is_visitor) {
-        //room button
-        if( _IMC.enable_room )ui.addApp("room", { discussion: false});
-        //notification button
-        if( _IMC.enable_noti )ui.addApp("notification");
+        if( _IMC.enable_room )ui.addApp("room", { discussion: (_IMC.discussion && !_IMC.is_visitor) });
+        if(_IMC.enable_noti )ui.addApp("notification");
     }
-    //setting button
-    ui.addApp("setting", {"data": webim.setting.defaults.data});
+    if(_IMC.enable_chatlink) ui.addApp("chatbtn");
+    ui.addApp("setting", {"data": webim.setting.defaults.data, "copyright": true});
     //render
 	ui.render();
 	//online
 	_IMC['is_login'] && im.autoOnline() && im.online();
 })(webim);
-
